1a86587e36ea9a1414f894e6691481de70404eb8
fcu-d0511322 <32889971+fcu-d0511322@users.noreply.github.com>
2017-12-27 16:17:11 +0800

    Create practice.js

---
+++ day1/D0511322/practice.js
@@ -0,0 +1,19 @@
+{
+  "name": "jenny",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "homework": "echo \"helloworld\""
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "axios": "^0.17.1",
+    "nodemon": "^1.13.3"
+  },
+  "devDependencies": {
+    "mocha": "^4.0.1"
+  }
+}
